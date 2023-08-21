6dab4bee72341f47a7f7c25180ffa71c7436a14f
ISLAMBEK <93197300+islambek97@users.noreply.github.com>
2023-08-21 17:17:45 +0600

    Update CostForm.js

--- src/components/NewCost/CostForm.js
+++ src/components/NewCost/CostForm.js
@@ -4,52 +4,78 @@ import './CostForm.css'
 
 function CostForm() {
 
-    // const [name, setName] = useState('')
-    // const [amount, setAmount] = useState('')
-    // const [date, setDate] = useState('')
+    const [inputName, setInputName] = useState('')
+    const [inputAmount, setInputAmount] = useState('')
+    const [inputDate, setInputDate] = useState('')
 
-    const [userInput, setUserInput] = useState({
-         name: '',
-         amount: '',
-         date: '',
-    })
+    // const [userInput, setUserInput] = useState({
+    //      name: '',
+    //      amount: '',
+    //      date: '',
+    // })
     function nameChangeHandler(event) {
-        // setName(event.target.value)
-        setUserInput({
-            ...userInput,
-            name: event.target.value
-        })
+        setInputName(event.target.value)
+
+        //2 способ
+        // setUserInput({
+        //     ...userInput,
+        //     name: event.target.value
+        // })
+
+        //3 способ
+        // setUserInput((previousState) => {
+        //     return{
+        //         ...previousState,
+        //         name: event.target.value
+        //     }
+        // })
     }
 
     function amountChangeHandler(event) {
-        // setAmount(event.target.value)
-        setUserInput({
-            ...userInput,
-            amount: event.target.value
-        })
+        setInputAmount(event.target.value)
+
+        // setUserInput({
+        //     ...userInput,
+        //     amount: event.target.value
+        // })
     }
 
     function dateChangeHandler(event) {
-        // setDate(event.target.value)
-        setUserInput({
-            ...userInput,
-            date: event.target.value
-        })
+        setInputDate(event.target.value)
+
+        // setUserInput({
+        //     ...userInput,
+        //     date: event.target.value
+        // })
     }
 
-    return <form>
+    function submitHandler(event){
+        //отключение перезагрузки страницы
+        event.preventDefault();
+        const costData = {
+            name: inputName,
+            amount: inputAmount,
+            date: new Date(inputDate)
+        };
+        console.log(costData)
+        setInputName('');
+        setInputAmount('');
+        setInputDate('');
+    }
+
+    return <form onSubmit={submitHandler}>
         <div className="new-cost__controls">
             <div className="new-cost__control">
                 <label>Название</label>
-                <input type="text" onChange={nameChangeHandler}/>
+                <input type="text" onChange={nameChangeHandler} value={inputName}/>
             </div>
             <div className="new-cost__control">
                 <label>Сумма</label>
-                <input type="number" min='0.01' step='0.01' onChange={amountChangeHandler}/>
+                <input type="number" min='0.01' step='0.01' onChange={amountChangeHandler} value={inputAmount}/>
             </div>
             <div className="new-cost__control">
                 <label>Дата</label>
-                <input type="date" min='2019-01-01' step='2023-12-31' onChange={dateChangeHandler}/>
+                <input type="date" min='2019-01-01' step='2023-12-31' onChange={dateChangeHandler} value={inputDate}/>
             </div>
             <div className="new-cost__actions">
                 <button type="submit">Добавить Расходы</button>
